950715497815f01bcaddab324b95a940c2772bd1
mcfrems <matthew.fremstad@gmail.com>
2023-01-18 19:43:31 -0600

    reviewed lecture

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+      //create variables here
+  const myName = "Matt";
+  const day = "Tuesday";
+  return (
+    <div>
+      <h1>Hello App</h1>
+      <p>My name is {myName}</p>
+      <p>Today is {day}</p>
+    </div>
+  )
+}
+
+export default Home
+
+//rfce
